ebc9e5176a46deb3273fd9d522acd5918b85f2c3
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-02-22 01:05:00 +0500

    now it has a showing arrow

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerInput: MonoBehaviour
 {
     [SerializeField] private Card selectedCard;
+    [SerializeField] private GameObject targetSelectorArrow;
 
     public void SetSelectedCard(Card card)
     {
@@ -14,7 +15,10 @@ public class PlayerInput: MonoBehaviour
     public void Update()
     {
         if (selectedCard == null)
+        {
+            targetSelectorArrow.SetActive(false);
             return;
+        }
 
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
@@ -23,5 +27,21 @@ public class PlayerInput: MonoBehaviour
         }
 
         print(selectedCard.gameObject.name);
+        targetSelectorArrow.SetActive(true);
+        PointArrowFromSelectedCardToMouse();
+    }
+
+    private void PointArrowFromSelectedCardToMouse()
+    {
+        Vector3 targetPosition = Input.mousePosition;
+        Vector3 objectPosition = targetSelectorArrow.transform.position;
+
+        Vector3 direction = targetPosition - objectPosition;
+        float magnitude = Vector3.Magnitude(targetPosition - objectPosition);
+        float angle = Vector2.SignedAngle(Vector2.right, direction);
+
+        targetSelectorArrow.transform.rotation = Quaternion.Euler(0, 0, angle);
+        var arrowsRectTransform = (targetSelectorArrow.transform as RectTransform);
+        arrowsRectTransform.sizeDelta = new Vector2(magnitude, arrowsRectTransform.sizeDelta.y);
     }
 }
