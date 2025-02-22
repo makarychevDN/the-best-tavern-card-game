060983c3b103d08605b8a4de2653dd0c59e5f986
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2025-02-22 11:06:05 +0500

    now level collects all card slots automatically

--- Assets/Game/Scripts/Level.cs
+++ Assets/Game/Scripts/Level.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Level : MonoBehaviour
 {
-    [SerializeField] private List<CardSlot> cardSlots;
     [SerializeField] private List<Card> cards;
     [SerializeField] private LastHoveredTargetContainer lastHoveredTargetContainer;
     [SerializeField] private PlayerInput playerInput;
+    private List<CardSlot> cardSlots;
 
     public LastHoveredTargetContainer LastHoveredTargetContainer => lastHoveredTargetContainer;
     public PlayerInput PlayerInput => playerInput;
@@ -14,6 +15,7 @@ public class Level : MonoBehaviour
     private void Awake()
     {
         playerInput.Init(this);
+        cardSlots = GetComponentsInChildren<CardSlot>().ToList();
         cardSlots.ForEach(slot => slot.Init(this));
         cards.ForEach(card => card.Init(this));
     }
